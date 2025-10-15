d92b8f8ac568e8e40fbdd30ba640a1d6efd0cba1
Quyen Anh <quyenanh10102005@gmail.com>
2025-10-15 22:18:46 +0700

    QA

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/ảnh', express.static(path.join(__dirname, 'public/ảnh')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 
 // Connect to Database
